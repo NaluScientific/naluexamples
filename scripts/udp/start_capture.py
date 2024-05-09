482d19b1adad1caaaa70cb472cd7405e446b9476
Marcus Luck <marcus.luck@outlook.com>
2024-05-09 11:25:32 -0700

    CHG: cleanup

--- scripts/udp/start_capture.py
+++ scripts/udp/start_capture.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
 import argparse
-import re
 import logging
-import socket
 import sys
 
+from helpers import _is_ipstr_valid, _parse_ip_str
 from naluconfigs import get_available_models
-from naludaq.board import Board, startup_board
+from naludaq.board import Board
 from naludaq.controllers import get_board_controller, get_readout_controller
+
 
 def main():
     """Starts data capture for a board"""
@@ -14,38 +15,30 @@ def main():
     if args.debug:
         setup_logger()
 
-    BOARD_MODEL = args.model
-    
+    board_model = args.model
+
     if not _is_ipstr_valid(args.board_ip):
         raise ValueError("Invalid format: Board IP")
-    BOARD_IP = _parse_ip_str(args.board_ip)
+    board_ip = _parse_ip_str(args.board_ip)
 
     if args.host_ip is None:
-        local_hostname = socket.gethostname()
-        ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
-        filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
-        HOST_IP = (filtered_ips[0], 4660)
+        host_ip = ("127.0.0.1", 4660)
     else:
         if not _is_ipstr_valid(args.host_ip):
             raise ValueError("Invalid format: Host IP")
-        HOST_IP = _parse_ip_str(args.host_ip)
+        host_ip = _parse_ip_str(args.host_ip)
 
-    BOARD = Board(BOARD_MODEL)
-    BOARD.get_udp_connection(BOARD_IP, HOST_IP)
+    atof = Board(board_model)
+    atof.get_udp_connection(board_ip, host_ip)
 
-    BC = get_board_controller(BOARD)
-    RC = get_readout_controller(BOARD)
+    get_readout_controller(atof).set_read_window(*args.read_window)
 
-    if args.read_window:
-        RC.set_read_window(*args.read_window)
-    if args.record_window:
-        RC.set_record_window(*args.record_window)
-
-    BC.start_readout(
+    get_board_controller(atof).start_readout(
         trig=args.trigger_mode,
         lb=args.lookback_mode,
     )
-    BOARD.disconnect()
+    atof.disconnect()
+
 
 def setup_logger(level=logging.DEBUG):
     """Setup a basic logger."""
@@ -64,11 +57,10 @@ def setup_logger(level=logging.DEBUG):
         logging.getLogger(name).setLevel(logging.CRITICAL)
     return logger
 
+
 def parse_args(argv):
     """Parse command line arguments"""
-    parser = argparse.ArgumentParser(
-        description="Inits a board to a default state"
-    )
+    parser = argparse.ArgumentParser(description="Inits a board to a default state")
     parser.add_argument(
         "--model",
         "-m",
@@ -94,41 +86,36 @@ def parse_args(argv):
     parser.add_argument(
         "--debug",
         "-d",
-        action='store_true',
+        action="store_true",
     )
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--read_window', nargs=3, type=int, help="Read Window in the format: num windows, lookback, write after trigger",)
-    group.add_argument('--record_window', nargs=2, type=int, help="Record Window in the format: Start window, record length (num windows)",)
+
+    parser.add_argument(
+        "--readout_window",
+        nargs=3,
+        type=int,
+        help="Read Window in the format: num windows, lookback, write after trigger",
+    )
 
     parser.add_argument(
         "--trigger_mode",
         "-t",
         type=str,
         required=True,
-        help="",
-        choices=["imm", "ext", "self"]
+        help="ext: External trigger, using the trig_in on the board or software commands\nimm: Immediate trigger will trigger automatically without signal\nself: Self trigger will trigger on analog signals.\n",
+        choices=["imm", "ext", "self"],
     )
 
     parser.add_argument(
         "--lookback_mode",
         "-l",
         type=str,
+        required=False,
         help="",
-        choices=["forced", "trig"]
+        choices=["forced", "trig"],
     )
 
     return parser.parse_args(argv)
 
-def _parse_ip_str(ip: str) -> tuple:
-    splitted = ip.split(":")
-    return (splitted[0], int(splitted[1]))
-
-
-def _is_ipstr_valid(ip: str):
-    ip_port_regex = r"^((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d):([1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$"
-    if re.match(ip_port_regex, ip):
-        return True
-    return False
 
 if __name__ == "__main__":
     main()
--- scripts/udp/stop_capture.py
+++ scripts/udp/stop_capture.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
 import argparse
-import re
 import logging
-import socket
 import sys
 
+from helpers import _is_ipstr_valid, _parse_ip_str
 from naluconfigs import get_available_models
 from naludaq.board import Board
 from naludaq.controllers import get_board_controller
+
 
 def main():
     """Starts data capture for a board"""
@@ -14,29 +15,26 @@ def main():
     if args.debug:
         setup_logger()
 
-    BOARD_MODEL = args.model
-    
+    board_model = args.model
+
     if not _is_ipstr_valid(args.board_ip):
         raise ValueError("Invalid format: Board IP")
-    BOARD_IP = _parse_ip_str(args.board_ip)
+    board_ip = _parse_ip_str(args.board_ip)
 
     if args.host_ip is None:
-        local_hostname = socket.gethostname()
-        ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
-        filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
-        HOST_IP = (filtered_ips[0], 4660)
+        host_ip = ("127.0.0.1", 4660)
     else:
         if not _is_ipstr_valid(args.host_ip):
             raise ValueError("Invalid format: Host IP")
-        HOST_IP = _parse_ip_str(args.host_ip)
+        host_ip = _parse_ip_str(args.host_ip)
 
-    BOARD = Board(BOARD_MODEL)
-    BOARD.get_udp_connection(BOARD_IP, HOST_IP)
+    atof = Board(board_model)
+    atof.get_udp_connection(board_ip, host_ip)
 
-    BC = get_board_controller(BOARD)
-    BC.stop_readout()
+    get_board_controller(atof).stop_readout()
 
-    BOARD.disconnect()
+    atof.disconnect()
+
 
 def setup_logger(level=logging.DEBUG):
     """Setup a basic logger."""
@@ -55,11 +53,10 @@ def setup_logger(level=logging.DEBUG):
         logging.getLogger(name).setLevel(logging.CRITICAL)
     return logger
 
+
 def parse_args(argv):
     """Parse command line arguments"""
-    parser = argparse.ArgumentParser(
-        description="Inits a board to a default state"
-    )
+    parser = argparse.ArgumentParser(description="Inits a board to a default state")
     parser.add_argument(
         "--model",
         "-m",
@@ -85,21 +82,11 @@ def parse_args(argv):
     parser.add_argument(
         "--debug",
         "-d",
-        action='store_true',
+        action="store_true",
     )
 
     return parser.parse_args(argv)
 
-def _parse_ip_str(ip: str) -> tuple:
-    splitted = ip.split(":")
-    return (splitted[0], int(splitted[1]))
-
-
-def _is_ipstr_valid(ip: str):
-    ip_port_regex = r"^((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d):([1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$"
-    if re.match(ip_port_regex, ip):
-        return True
-    return False
 
 if __name__ == "__main__":
     main()
