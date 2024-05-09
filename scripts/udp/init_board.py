1c5660872334d49efc57ddb6c35ca16b34455179
Marcus Luck <marcus.luck@outlook.com>
2024-05-09 09:52:45 -0700

    FIX: variables are not contants

--- scripts/udp/init_board.py
+++ scripts/udp/init_board.py
@@ -8,36 +8,37 @@ from naluconfigs import get_available_models
 from naludaq.board import Board, startup_board
 from naludaq.controllers import get_board_controller
 
+
 def main():
     """Inits the board to a default state"""
     args = parse_args(sys.argv[1:])
     if args.debug:
         setup_logger()
 
-    BOARD_MODEL = args.model
-    
+    board_model = args.model
+
     if not _is_ipstr_valid(args.board_ip):
         raise ValueError("Invalid format: Board IP")
-    BOARD_IP = _parse_ip_str(args.board_ip)
+    board_ip = _parse_ip_str(args.board_ip)
 
     if args.host_ip is None:
         local_hostname = socket.gethostname()
         ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
         filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
-        HOST_IP = (filtered_ips[0], 4660)
+        host_ip = (filtered_ips[0], 4660)
     else:
         if not _is_ipstr_valid(args.host_ip):
             raise ValueError("Invalid format: Host IP")
-        HOST_IP = _parse_ip_str(args.host_ip)
+        host_ip = _parse_ip_str(args.host_ip)
 
-    BOARD = Board(BOARD_MODEL)
-    BOARD.get_udp_connection(BOARD_IP, HOST_IP)
+    atof = Board(board_model)
+    atof.get_udp_connection(board_ip, host_ip)
 
-    BC = get_board_controller(BOARD)
-    BC.reset_board()
+    get_board_controller(atof).reset_board()
 
-    startup_board(BOARD)
-    BOARD.disconnect()
+    startup_board(atof)
+    atof.disconnect()
+
 
 def setup_logger(level=logging.DEBUG):
     """Setup a basic logger."""
@@ -56,11 +57,10 @@ def setup_logger(level=logging.DEBUG):
         logging.getLogger(name).setLevel(logging.CRITICAL)
     return logger
 
+
 def parse_args(argv):
     """Parse command line arguments"""
-    parser = argparse.ArgumentParser(
-        description="Inits a board to a default state"
-    )
+    parser = argparse.ArgumentParser(description="Inits a board to a default state")
     parser.add_argument(
         "--model",
         "-m",
@@ -86,9 +86,10 @@ def parse_args(argv):
     parser.add_argument(
         "--debug",
         "-d",
-        action='store_true',
+        action="store_true",
     )
     return parser.parse_args(argv)
+
 
 def _parse_ip_str(ip: str) -> tuple:
     splitted = ip.split(":")
@@ -100,6 +101,7 @@ def _is_ipstr_valid(ip: str):
     if re.match(ip_port_regex, ip):
         return True
     return False
+
 
 if __name__ == "__main__":
     main()
